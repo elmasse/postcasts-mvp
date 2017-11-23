1d6a5b05d19c31a6f96a4cc7b2ff2b0320a6c7f2
Max Fierro <elmasse@gmail.com>
2017-11-22 21:55:47 -0300

    load-form refactor

--- src/app.js
+++ src/app.js
@@ -8,9 +8,6 @@ import PostCast from './components/postcast'
 import LoadForm from './components/load-form'
 import ErrorModal from './components/error-modal'
 
-import ReactIcon from './components/icons/react'
-import ReduxIcon from './components/icons/redux'
-
 export default class App extends Component {
   
   constructor(props){
@@ -38,14 +35,6 @@ export default class App extends Component {
     })
   }
 
-  loadReact = () => {
-    this.handleSourceSelection('https://raw.githubusercontent.com/facebook/react/master/README.md')
-  }
-
-  loadRedux = () => {
-    this.handleSourceSelection('https://raw.githubusercontent.com/reactjs/redux/master/README.md')
-  }
-
   render() {
     const { src, error } = this.state
 
@@ -55,9 +44,9 @@ export default class App extends Component {
         <Navigation />
         <div className="App-header">
           <LoadForm onSelected={this.handleSourceSelection}/>
-          <span>Or try these README.md from github</span>
-          <ReactIcon onClick={this.loadReact} height="40" width="40"/>
-          <ReduxIcon onClick={this.loadRedux} height="40" width="40" />
+          {/* <span>Or try these README.md from github</span>
+          <ReactIcon onClick={this.loadReact} height="40" width="40" />
+          <ReduxIcon onClick={this.loadRedux} height="40" width="40" /> */}
         </div>
         {!error && <PostCast className="PostCast" src={src}/>}
       </Wrapper>
@@ -77,12 +66,6 @@ const Wrapper = styled.div`
     align-items: center;
     justify-content: center;
     padding: 20px;
-
-    span {
-      font-size: 14px;
-      margin: 0 20px;
-      font-weight: 100;
-    }
   }
 
   & .PostCast {
--- src/components/load-form.js
+++ src/components/load-form.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
-import styled from 'react-emotion'
+import styled, { keyframes } from 'react-emotion'
+
+import ReactIcon from './icons/react'
+import ReduxIcon from './icons/redux'
+
 
 export default class LoadForm extends Component {
   
@@ -36,30 +40,77 @@ export default class LoadForm extends Component {
     onSelected(src)
   }
 
+  handleFocus = () => {
+    this.setState({focused: true})
+  }
+
+  handleBlur = () => {
+    
+    this.setState(({src, focused}) => ({focused: !!src}) )
+  }
+
+
+  loadReact = () => {    
+    this.props.onSelected('https://raw.githubusercontent.com/facebook/react/master/README.md')
+  }
+
+  loadRedux = () => {
+    this.props.onSelected('https://raw.githubusercontent.com/reactjs/redux/master/README.md')
+  }
+  
   render() {
-    const { src } = this.state
+    const { src, focused } = this.state
     return (
-      <div>
-        <Input 
+      <Form focused={focused}>
+        <Input
+          className={focused ? 'expanded' : ''}
           name="src"
           value={src}
           placeholder="url to markdown file"
           onChange={this.handleInputChange}
           onKeyPress={this.handleKeyPress}
+          onFocus={this.handleFocus}
+          onBlur={this.handleBlur}
         />
-      </div>
+        <Message>Or try these README.md from github</Message>
+        <ReactIcon onClick={this.loadReact} height="40" width="40" />
+        <ReduxIcon onClick={this.loadRedux} height="40" width="40" />
+        
+      </Form>
     )
   }
 
 }
 
+const Form = styled.div`
+  display: flex;
+  align-items: center;
+
+  > svg, > div {
+    display: ${ ({focused}) => focused ? 'none' : 'block' };
+  }
+`
+
+
 const Input = styled.input`
   border: none;
   border-bottom: 1px solid #424242;
   background: rgba(0,0,0, 0);
-  font-size: 32px;
+  font-size: ${({ value }) => 32 - (value.length / 6)}px;
+  height: 42px;
   color: #f1f1f1;
   font-weight: 100;
   outline-color: #222;
-  min-width: 400px;
+  width: 400px;
+
+  &.expanded {
+    width: 800px;
+  }
+`
+
+const Message = styled.div`
+  font-size: 14px;
+  margin: 0 20px;
+  font-weight: 100;
+
 `
