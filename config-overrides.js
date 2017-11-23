02bf24b62de0f93c9cccb36a3566b49c3fe39e56
Max Fierro <elmasse@gmail.com>
2017-11-23 10:36:44 -0300

    intent to remove the fucking service-worker

--- config-overrides.js
+++ config-overrides.js
@@ -2,5 +2,8 @@ const { injectBabelPlugin } = require('react-app-rewired');
 
 module.exports = function override(config, env) {
   config = injectBabelPlugin('emotion', config);
+
+  config.plugins = config.plugins.filter(p => p.constructor.name !== 'SWPrecacheWebpackPlugin')
+
   return config;
 };
